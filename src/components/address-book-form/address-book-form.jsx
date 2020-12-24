9d91ce0d228e2da97ce9c8d049b2c170c8412306
Aditya Verma <aaadarths2357@gmail.com>
2020-12-24 18:23:55 +0530

    Added: implementation to edit and remove contact details the action buttons on the home page by using axios service to connect to the spring backend

--- src/components/address-book-form/address-book-form.jsx
+++ src/components/address-book-form/address-book-form.jsx
@@ -35,43 +35,43 @@ const initialState = {
   }
 }
 class AddressBookForm extends React.Component {
-constructor(props) {
-super(props);
-this.state = {
-  fullName: '',
-  address: '',
-  locationInfo: {
-    "Assam": ["Dispur", "Guwahati"],
-    "Gujarat": ["Vadodara", "Surat"],
-    "Uttar Pradesh": ["Lucknow", "Kanpur"],
-    "Madhya Pradesh": ["Bhopal", "Jabalpur"]
-  },
-  city: '',
-  state: '',
-  zip: '',
-  phoneNumber: '',
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: '',
+      address: '',
+      locationInfo: {
+        "Assam": ["Dispur", "Guwahati"],
+        "Gujarat": ["Vadodara", "Surat"],
+        "Uttar Pradesh": ["Lucknow", "Kanpur"],
+        "Madhya Pradesh": ["Bhopal", "Jabalpur"]
+      },
+      city: '',
+      state: '',
+      zip: '',
+      phoneNumber: '',
 
-  id: '',      
-  isUpdate: false,
-  isError: false,
+      id: '',      
+      isUpdate: false,
+      isError: false,
 
-  error: {
-    fullName: '',
-    address: '',
-    city: '',
-    state: '',
-    zip: '',
-    phoneNumber: ''
-  },  
-  valid: {
-    fullName: '',
-    address: '',
-    city: '',
-    state: '',
-    zip: '',
-    phoneNumber: ''
-  }
-}
+      error: {
+        fullName: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+        phoneNumber: ''
+      },  
+      valid: {
+        fullName: '',
+        address: '',
+        city: '',
+        state: '',
+        zip: '',
+        phoneNumber: ''
+      }
+    }
     this.nameChangeHandler = this.nameChangeHandler.bind(this);
     this.phoneNumberChangeHandler = this.phoneNumberChangeHandler.bind(this);
     this.addressChangeHandler = this.addressChangeHandler.bind(this);
@@ -79,6 +79,36 @@ this.state = {
     this.stateChangeHandler = this.stateChangeHandler.bind(this);
     this.zipChangeHandler = this.zipChangeHandler.bind(this);
   }
+
+  componentDidMount = () => {
+    let id = this.props.match.params.id;
+    if(id !== undefined && id!=='') {
+      this.getContactById(id);
+    }
+  }
+
+  getContactById = (id) => {
+    new AddressBookService().getContactById(id)
+    .then(responseDTO => {
+      let responseText = responseDTO.data;
+      this.setContactData(responseText.data);
+    }).catch(error => {
+      console.log("Error while fetching contact data by ID :\n" + JSON.stringify(error));
+    })
+  }
+  setContactData = (contact) => {
+    this.setState({
+      id: contact.id,
+      fullName: contact.fullName,
+      address: contact.address,
+      city: contact.city,
+      state: contact.state,
+      zip: contact.zip,
+      phoneNumber: contact.phoneNumber,
+      isUpdate: true
+    });
+  }
+
   nameChangeHandler = (event) => {
     this.setState({fullName: event.target.value});
     this.checkName(event.target.value);
@@ -103,6 +133,7 @@ this.state = {
     this.setState({zip: event.target.value});
     this.checkZip(event.target.value);
   }
+
   initializeMessage = (field, errorMessage, validMessage) => {
     this.setState(previousState => ({
       error: {
@@ -121,7 +152,7 @@ this.state = {
     if(nameValue.length === 0) {
       this.initializeMessage('fullName', '', '');
     } else {
-      const NAME_REGEX = RegExp("^[A-Z]{1}[a-z]{2,}([ ][A-Z]{1}[a-z]{2,})?$");
+      const NAME_REGEX = RegExp("^[A-Z]{1}[a-z]{2,}[ ][A-Z]{1}[a-z]{2,}$");
       if(NAME_REGEX.test(nameValue)) {
         this.initializeMessage('fullName', '', '✓');
       } else {
@@ -211,16 +242,27 @@ this.state = {
         zip: this.state.zip,
         phoneNumber: this.state.phoneNumber
       }
-      new AddressBookService().addContact(contactObject)
-      .then(responseDTO => {
-        let responseText = responseDTO.data;
-        alert("Contact Added Successfully!!!\n" + JSON.stringify(responseText.data));
+      if(this.state.isUpdate) {
+        new AddressBookService().updateContact(contactObject)
+        .then(responseText => {
+          alert("Contact Updated Successfully!!!\n" + JSON.stringify(responseText.data));
+          this.reset();
+          this.props.history.push("/home");
+        }).catch(error => {
+          console.log("Error while updating Contact!!!\n" + JSON.stringify(error));
+        })
+      } else {
+        new AddressBookService().addContact(contactObject)
+        .then(responseDTO => {
+          let responseText = responseDTO.data;
+          alert("Contact Added Successfully!!!\n" + JSON.stringify(responseText.data));
+          this.reset();
+          this.props.history.push("/home");
+        }).catch(error => {
+          console.log("Error while adding Contact!!!\n" + JSON.stringify(error));
+        });
         this.reset();
-        this.props.history.push("/home");
-      }).catch(error => {
-        console.log("Error while adding Contact!!!\n" + JSON.stringify(error));
-      });
-      this.reset();
+      }
     }
   }
 
@@ -310,7 +352,7 @@ this.state = {
                 </div>
                 <div className="buttonParent">
                     <div className="submit-reset">
-                        <button type="submit" className="button submitButton">Add</button>
+                        <button type="submit" className="button submitButton">{this.state.isUpdate ? 'Update' : 'Add'}</button>
                         <button type="reset" className="resetButton button">Reset</button>
                     </div>
                 </div>
